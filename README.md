867832add1883151f716a05a7095188222397ba6
Polokalap <116758671+Polokalap@users.noreply.github.com>
2024-06-05 13:11:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# example-plugin
+An example plugin for the PyMine plugin system, more will be added as more is added to the plugin api!
---
+++ example_plugin/__init__.py
@@ -0,0 +1,54 @@
+from pymine.types.abc import AbstractPlugin
+from pymine.server import server
+
+
+class MyExamplePlugin(AbstractPlugin):
+    def __init__(self, plugin_yml):
+        self.config = plugin_yml
+
+    @server.api.register.on_packet("handshaking", 0x00)
+    async def my_packet_handler(stream, packet):
+        server.console.debug("EXAMPLE PLUGIN: Handshake packet received!")
+
+    @server.api.register.on_server_start
+    async def my_server_start_handler():
+        server.console.debug("EXAMPLE PLUGIN: Ayyy server do be ready tho ngl")
+
+    @server.api.register.on_server_stop
+    async def my_server_stop_handler():
+        server.console.debug("EXAMPLE PLUGIN: bishhh you better be starting this server right back up or else")
+
+    async def teardown(self):
+        server.console.debug("EXAMPLE PLUGIN: being torn down...")
+
+
+async def setup(plugin_yml):
+    server.api.add_plugin(MyExamplePlugin(plugin_yml))
+
+
+# from pymine.server import server
+#
+#
+# @server.api.events.on_packet("handshaking", 0x00)
+# async def example_handle_handshake(stream, packet):
+#     server.console.debug("EXAMPLE PLUGIN: Handshake packet received!")
+#
+#     return True, stream
+#
+#
+# @server.api.events.on_server_ready
+# async def on_server_ready():
+#     server.console.info("EXAMPLE PLUGIN: AYYY SERVER DO BE READY THO NGL?")
+#
+#
+# @server.api.events.on_server_stop
+# async def on_server_stop():
+#     server.console.info("EXAMPLE PLUGIN: bish you better start this server back up")
+#
+#
+# async def setup(server, plugin_yml):
+#     server.console.info("EXAMPLE PLUGIN: setup coroutine called!")
+#
+#
+# async def teardown(server):
+#     server.console.info("EXAMPLE PLUGIN: teardown coroutine!")
